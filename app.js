f8f7d96ee4b79ab745b0e3c28ead4d758b0f0a54
Vy Tran <vtran1022@Gmail.com>
2021-04-08 19:03:51 -0700

    implementing organization of code

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 $(document).ready(function(){
+  // Select already existing elements
   var $app = $('#app');
   $app.html('');
 
+  // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
-  $title.appendTo($app);
-  $title.on("click", function(event) {
-    console.log(event);
-    alert('The title of this page is: ' + event.target.innerText);
-  });
-
   var $updateButton = $('<button id="update-feed">Update Feed</button>');
-  $updateButton.appendTo($app);
-  $updateButton.on("click", function(event) {
-    $feed.empty();
-    renderFeed();
-  });
-
   var $feed = $('<div id="feed"></div>');
-  $feed.appendTo($app);
+
+  // Create event handler functions
+  var handleTitleClick = function(event) {
+    alert('The title of this page is: ' + event.target.innerText);
+  }
 
   var renderFeed = function () {
-     var index = streams.home.length - 1;
+    var index = streams.home.length - 1;
     while(index >= 0) {
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
       $tweet.text('@' + tweet.user + ': ' + tweet.message);
       $tweet.appendTo($feed);
       index -= 1;
-   }
+    }
   };
+ renderFeed();
 
-  renderFeed();
+  // Set event listeners (providing appropriate handlers as input)
+  $title.on('click', handleTitleClick);
+  $updateButton.on("click", function(event) {
+    $feed.empty();
+    renderFeed();
+  });
+
+  // Append new HTML elements to the DOM
+  $title.appendTo($app);
+  $updateButton.appendTo($app);
+  $feed.appendTo($app);
+
 });
 
