afc8d0551b6cb9b3d30e821e4e0f511ac3458b0c
Vy Tran <vtran1022@Gmail.com>
2021-04-08 19:43:08 -0700

    committing changes to code so far for tweet div

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   var $title = $('<h1>Twiddler</h1>');
   var $updateButton = $('<button id="update-feed">Update Feed</button>');
   var $feed = $('<div id="feed"></div>');
+  var $tweet = $('<div class="tweet"></div>');
 
   // Create event handler functions
   var handleTitleClick = function(event) {
@@ -18,10 +19,20 @@ $(document).ready(function(){
     while(index >= 0) {
       var tweet = streams.home[index];
       var $tweet = $('<div class="tweet"></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      // $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      // $tweet.appendTo($feed);
+
+      var $profilePhoto = $('<img class="profile-photo" src="assets/img/mracus.png"></img>');
+      $profilePhoto.appendTo($tweet);
+      var $username = $('<span class="username"> @' + tweet.user + '</span>');
+      $username.appendTo($tweet);
+      var $message = $('<p class ="message">' + tweet.message + '</p>');
+      $message.appendTo($tweet);
+
       $tweet.appendTo($feed);
       index -= 1;
     }
+
   };
  renderFeed();
 
@@ -37,5 +48,22 @@ $(document).ready(function(){
   $updateButton.appendTo($app);
   $feed.appendTo($app);
 
+
+
 });
 
+/* <div class="tweet">
+
+  <img class="profile-photo" src="assets/img/'username'.png"></img>
+  <span class="username"></span>
+  <p class ="message"></p>
+  <span class="timestamp"></span>
+  <div class="icon">
+    <img class="icon comment" src="assets/icons/placeholder.png"></img>
+    <img class="icon retweet" src="assets/icons/placeholder.png"></img>
+    <img class="icon like" src="assets/icons/placeholder.png"></img>
+    <img class="icon share" src="assets/icons/placeholder.png"></img>
+  </div>
+
+
+</div> */
